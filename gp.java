614bc5348c6c1ed24341e308729f8a7cb3258307
Daniel Ryu <d97shryu@gmail.com>
2019-02-14 17:04:20 -0500

    started gp programming

---
+++ gp.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.Random;
+
+
+public class gp {
+    private TreeMaker tester;
+    private Tree test;
+    public int c = 0;
+    private boolean 
+    public gp() {
+        
+    }
+
+    public void countNodes(Tree r, )
+}
